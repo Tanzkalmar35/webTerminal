9502708c7602d7e64368aefdc33a4447204435d4
Tanzkalmar35 <94364725+Tanzkalmar35@users.noreply.github.com>
2023-04-26 23:26:15 +0200

    beginning the setup, svelte running, tailwind is ass

---
+++ webTerminal/src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    let count = 0;
+
+    function increaseCount() {
+        count += 1;
+        console.log(count);
+    }
+
+</script>
+
+<button on:click={increaseCount}>
+    CLick me {count}
+</button>
