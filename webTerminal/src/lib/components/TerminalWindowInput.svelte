5f97a57c0e6c58e311d0ee82c6b2d48731d5a9a9
Tanzkalmar35 <94364725+Tanzkalmar35@users.noreply.github.com>
2023-04-29 18:55:09 +0200

    implementing input and applying first theme

---
+++ webTerminal/src/lib/components/TerminalWindowInput.svelte
@@ -0,0 +1,8 @@
+<div data-theme="dracula" class="absolute flex bottom-[2.5vh] w-[90vw] m-[2.5vh]">
+    <p class="flex items-center px-5 text-lg">
+        <span data-theme="dracula" class="text-primary">welcome</span>
+        <span data-theme="dracula">@</span>
+        <span data-theme="dracula" class="text-secondary">portfolio</span>
+    </p>
+    <input data-theme="dracula" class="w-full p-5 text-lg focus:outline-none"/>
+</div>
--- webTerminal/src/routes/+page.svelte
+++ webTerminal/src/routes/+page.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
+    import TerminalWindow from "../lib/components/TerminalWindowInput.svelte";
 </script>
 
-<div data-theme="luxury" class="min-w-[100vw] min-h-[100vh]">
-
+<div data-theme="dracula" class="min-w-[100vw] min-h-[100vh] flex items-center justify-center">
+    <div class="w-[98vw] h-[98vh] outline">
+        <TerminalWindow />
+    </div>
 </div>
