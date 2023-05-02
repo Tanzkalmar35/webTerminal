5f59606ad7e9e40fe283d7372d2fba39527392e0
Tanzkalmar35 <94364725+Tanzkalmar35@users.noreply.github.com>
2023-05-02 22:05:37 +0200

    expanding input field features

--- webTerminal/src/lib/components/TerminalWindowInput.svelte
+++ webTerminal/src/lib/components/TerminalWindowInput.svelte
@@ -1,8 +1,43 @@
-<div data-theme="dracula" class="absolute flex bottom-[2.5vh] w-[90vw] m-[2.5vh]">
-    <p class="flex items-center px-5 text-lg">
+<script lang="ts">
+
+    let terminalInputElement;
+
+    let currentCommand: string = "";
+
+    const allCommands: Array<string> = [
+        "help",
+        "about",
+        "projects",
+        "contact",
+        "clear",
+        "exit",
+    ];
+
+    function commandSubmit(e: KeyboardEvent) {
+        if (e.code === "Enter") {
+            currentCommand = terminalInputElement.value;
+            allCommands.includes(currentCommand) ? console.log(currentCommand + " Command submitted") : "";
+
+            terminalInputElement.value = "";
+        }
+    }
+
+</script>
+
+
+<div data-theme="dracula" class="absolute flex bottom-[0] w-[90vw] m-[2.5vh]">
+    <p class="flex items-center pl-5 text-lg">
         <span data-theme="dracula" class="text-primary">welcome</span>
         <span data-theme="dracula">@</span>
         <span data-theme="dracula" class="text-secondary">portfolio</span>
+        <span data-theme="dracula">:$</span>
+        <span data-theme="dracula" class="ml-3">~</span>
     </p>
-    <input data-theme="dracula" class="w-full p-5 text-lg focus:outline-none"/>
+    <input
+            bind:this={terminalInputElement}
+            autofocus
+            data-theme="dracula"
+            class="w-full p-5 text-lg focus:outline-none"
+            on:keydown={commandSubmit}
+    />
 </div>
