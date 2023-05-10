7bd5770153ba1da4f45727650b57d2bd38dc0be1
Tanzkalmar35 <94364725+Tanzkalmar35@users.noreply.github.com>
2023-05-10 12:20:46 +0200

    Clear command fully implemented

---
+++ webTerminal/src/lib/components/Terminal.svelte
@@ -0,0 +1,25 @@
+<script>
+    import {clearTerminal, shellTexts} from "$lib/stores/Store";
+    import TerminalWindowInput from "$lib/components/TerminalWindowInput.svelte";
+
+    let initialCmdInput;
+
+    $: if ($clearTerminal) {
+        initialCmdInput.value = ""
+        initialCmdInput.removeAttribute("disabled")
+        initialCmdInput.focus()
+    }
+
+</script>
+
+<div>
+    <TerminalWindowInput bind:cmdInput={initialCmdInput}/>
+    <ul>
+        {#each $shellTexts as shellText}
+            <li>
+                <p>{shellText}</p>
+                <TerminalWindowInput/>
+            </li>
+        {/each}
+    </ul>
+</div>
--- webTerminal/src/routes/+page.svelte
+++ webTerminal/src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import TerminalWindowInput from "../lib/components/TerminalWindowInput.svelte";
     import {clearTerminal, shellTexts} from "$lib/stores/Store";
+    import Terminal from "$lib/components/Terminal.svelte";
 
     // The banner text that gets rendered when the shell is first loaded
     const asciiNameBanner =
@@ -13,12 +14,12 @@
         "███        ███   █████    ███    ████    ███     ████  ██  ███   █████    ██        ███     ████  ██  ███       ███         ███ ████ ███ ███   ███   ███ ████ ████  ███ \n \n \n" +
             "Type help to get started..."
 
-    export let initialCmdInput;
     let terminalDiv;
+    let bannerVisible = true;
 
     $: if ($clearTerminal) {
 
-        terminalDiv.innerHTML = '<ul>{#each $shellTexts as shellText}<li><p>{shellText}</p><TerminalWindowInput/></li>{/each}</ul>';
+        bannerVisible = false;
 
         clearTerminal.set(false);
     }
@@ -27,15 +28,9 @@
 
 <div data-theme="dracula" class="min-w-[100vw] min-h-[100vh] flex items-center justify-center">
     <div bind:this={terminalDiv} class="w-[97vw] h-[97vh] outline pl-[5vh] pt-[2vh] overflow-hidden">
-        <pre>{asciiNameBanner}</pre>
-        <TerminalWindowInput bind:this={initialCmdInput} />
-            <ul>
-                {#each $shellTexts as shellText}
-                    <li>
-                        <p>{shellText}</p>
-                        <TerminalWindowInput />
-                    </li>
-                {/each}
-            </ul>
+        {#if bannerVisible}
+            <pre>{asciiNameBanner}</pre>
+        {/if}
+        <Terminal />
     </div>
 </div>
